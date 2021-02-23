f3d47b62d1f06255f9e6197eab13f8552bd88051
daLLyw0w <stefanbuciujr@gmail.com>
2021-02-23 15:01:29 +0200

    Initial commit

---
+++ bsir2465/src/main/java/bsir2465/App.java
@@ -0,0 +1,13 @@
+package bsir2465;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ bsir2465/src/test/java/bsir2465/AppTest.java
@@ -0,0 +1,20 @@
+package bsir2465;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
