86c6f4180db093b9a506aa62def90ec60640c85d
vladcerbu <62288258+vladcerbu@users.noreply.github.com>
2021-04-03 19:38:02 +0300

    Done Coverage for Lab3

--- bsir2465/src/test/java/tasks/utils/TasksOperationsTest.java
+++ bsir2465/src/test/java/tasks/utils/TasksOperationsTest.java
@@ -1,6 +1,5 @@
 package tasks.utils;
 
-import javafx.collections.ObservableList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
